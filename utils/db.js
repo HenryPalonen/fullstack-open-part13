1bd7fe43aedc4ce0e3611cc6f3f13ce4369442c3
Henry Palonen <henry.palonen@alsus.fi>
2023-12-13 14:44:44 +0200

    Tasks 13.5-13.7

--- utils/db.js
+++ utils/db.js
@@ -16,7 +16,7 @@ const sequelize = new Sequelize(DATABASE_URL, {
 const connect = async () => {
     try {
       await sequelize.authenticate();
-   //   await runMigrations();
+//      await runMigrations();
       console.log('Connection has been established successfully.');
     } catch (error) {
       console.error('Unable to connect to the database:', error);
